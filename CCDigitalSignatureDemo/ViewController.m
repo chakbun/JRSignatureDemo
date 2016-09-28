f59867a0ebdf9f876360331136973eddeb6686cd
chakbun <cheungjaben@hotmail.com>
2016-09-28 23:43:23 +0800

    fix size of signature

--- CCDigitalSignatureDemo/ViewController.m
+++ CCDigitalSignatureDemo/ViewController.m
@@ -11,8 +11,8 @@
 #import "JRSignatureController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UIImageView *showImageView;
-@property (nonatomic, assign) BOOL haveSignature;
 
 @end
 
@@ -20,9 +20,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.haveSignature = NO;
     self.showImageView.image = [UIImage imageWithPDFNamed:@"myOrderPDF" fitInSize:self.showImageView.bounds.size];
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(go2Signature)];
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(go2Signature:)];
     [self.view addGestureRecognizer:tapGesture];
     UIBarButtonItem *exportItem = [[UIBarButtonItem alloc] initWithTitle:@"Export" style:UIBarButtonItemStyleDone target:self action:@selector(exportAction)];
     self.navigationItem.rightBarButtonItem = exportItem;
@@ -41,28 +40,26 @@
     CGContextRef pdfContext = UIGraphicsGetCurrentContext();
     [ self.showImageView.layer renderInContext:pdfContext];
     UIGraphicsEndPDFContext();
-//    BOOL writeResult = [pdfData writeToFile:@"/signatured.pdf" atomically:YES];
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *path = paths[0];
     NSError *error;
     BOOL writeResult = [pdfData writeToFile:[NSString stringWithFormat:@"%@/%@",path,@"signatured.pdf"] options:NSDataWritingAtomic error:&error];
     NSLog(@"============ result:%d err:%@============",writeResult,error);
-
 }
 
-- (void)go2Signature {
-    if (!self.haveSignature) {
-        JRSignatureController *signatureController = [[JRSignatureController alloc] init];
-        __weak __typeof(self) weakSelf = self;
-        
-        signatureController.completedSignatureBlock = ^(UIImage *signatureImage) {
-            UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 200)];;
-            imageView.image = signatureImage;
-            [weakSelf.showImageView addSubview:imageView];
-            weakSelf.haveSignature = YES;
-        };
-        [self.navigationController pushViewController:signatureController animated:YES];
-    }
+- (void)go2Signature:(UITapGestureRecognizer *)tapGesture {
+    CGPoint touchPoint = [tapGesture locationInView:self.showImageView];
+    JRSignatureController *signatureController = [[JRSignatureController alloc] init];
+    __weak __typeof(self) weakSelf = self;
+    
+    signatureController.completedSignatureBlock = ^(UIImage *signatureImage) {
+        CGFloat width = signatureImage.size.width * 100.0 / signatureImage.size.height;
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, width, 100)];
+        imageView.center = touchPoint;
+        imageView.image = signatureImage;
+        [weakSelf.showImageView addSubview:imageView];
+    };
+    [self.navigationController pushViewController:signatureController animated:YES];
 }
 
 
